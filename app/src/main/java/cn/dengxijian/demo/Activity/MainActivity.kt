a0a18a02f5ae2850c51708fa050905924c0c3d9a
dengxijian <923124803@qq.com>
2019-08-14 02:21:36 +0800

    新增下拉加载,上拉刷新

--- app/src/main/java/cn/dengxijian/demo/Activity/MainActivity.kt
+++ app/src/main/java/cn/dengxijian/demo/Activity/MainActivity.kt
@@ -2,6 +2,8 @@ package cn.dengxijian.demo.Activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
@@ -9,39 +11,110 @@ import cn.dengxijian.demo.CallBack.MyItemTouchCallBack
 import cn.dengxijian.demo.Listenner.OnStartDragListener
 import cn.dengxijian.demo.Adapter.RecycleViewAdapter
 import cn.dengxijian.demo.Divider.TextItemDecoration
-import cn.dengxijian.demo.R
 import kotlinx.android.synthetic.main.recycleview.*
+import android.os.Looper
 
-class MainActivity : AppCompatActivity(), OnStartDragListener {
+
+
+
+class MainActivity : AppCompatActivity(), OnStartDragListener, SwipeRefreshLayout.OnRefreshListener {
+    override fun onRefresh() {
+        refreshLayout.isRefreshing = true
+        resetData()
+        mHandler.postDelayed({ refreshLayout.isRefreshing = false }, 1000)
+    }
+
     override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
         itemHelper.startDrag(viewHolder)
     }
 
 
     lateinit var adapter: RecycleViewAdapter
-    var dataList = ArrayList<String>()
-    lateinit var itemHelper :ItemTouchHelper
+    lateinit var itemHelper: ItemTouchHelper
+    private var lastVisibleItem = 0
+    lateinit var linearLayoutManager: LinearLayoutManager
+    lateinit var refreshLayout : SwipeRefreshLayout
+    private val mHandler = Handler(Looper.getMainLooper())
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recycleview)
+        setContentView(cn.dengxijian.demo.R.layout.recycleview)
         initView()
     }
 
+
     fun initView() {
 
-        initData()
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        adapter = RecycleViewAdapter(this, this, dataList)
+        linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = linearLayoutManager
+        adapter = RecycleViewAdapter(this, this, initData())
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(TextItemDecoration())
         itemHelper = ItemTouchHelper(MyItemTouchCallBack(adapter))
         itemHelper.attachToRecyclerView(recyclerView)
+        refreshLayout =  findViewById(cn.dengxijian.demo.R.id.swipeRefresh)
+        refreshLayout.setColorSchemeResources(
+            android.R.color.holo_blue_light, android.R.color.holo_red_light,
+            android.R.color.holo_orange_light, android.R.color.holo_green_light
+        )
+        refreshLayout.setOnRefreshListener(this)
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    // 如果没有隐藏footView，那么最后一个条目的位置就比我们的getItemCount少1，自己可以算一下
+                    if (adapter.isFadeTips() === false && lastVisibleItem + 1 === adapter.itemCount) {
+                        mHandler.postDelayed({
+                            updateList()
+                        }, 500)
+                    }
+
+                    // 如果隐藏了提示条，我们又上拉加载时，那么最后一个条目就要比getItemCount要少2
+                    if (adapter.isFadeTips() === true && lastVisibleItem + 2 === adapter.itemCount) {
+                        mHandler.postDelayed({
+                            resetData()
+                        }, 500)
+                    }
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                // 在滑动完成后，拿到最后一个可见的item的位置
+                lastVisibleItem = linearLayoutManager.findLastCompletelyVisibleItemPosition()
+            }
+        })
     }
 
-    fun initData() {
-        for(i in 0 ..100){
-            dataList.add("item $i")
+
+    fun initData() : ArrayList<String> {
+        var data : ArrayList<String> = ArrayList()
+        for (i in 0..10) {
+            data.add("item $i")
         }
+        return data
     }
+
+    private fun updateList() {
+        var mDataList = adapter.getDataList()
+        if (mDataList.size < 30) {
+            for (i in 1..9) {
+                mDataList.add("${mDataList.size + 1}")
+            }
+            adapter.setHasMore(true)
+        } else {
+            adapter.setHasMore(false)
+        }
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun resetData() {
+        adapter.setInitData(initData())
+        adapter.notifyDataSetChanged()
+        adapter.setHasMore(true)
+        adapter.setFadeTips(false)
+    }
+
 }
--- app/src/main/java/cn/dengxijian/demo/Adapter/RecycleViewAdapter.kt
+++ app/src/main/java/cn/dengxijian/demo/Adapter/RecycleViewAdapter.kt
@@ -1,42 +1,112 @@
 package cn.dengxijian.demo.Adapter
 
 import android.content.Context
+import android.os.Handler
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import cn.dengxijian.demo.Listenner.OnStartDragListener
+import android.support.v4.os.HandlerCompat.postDelayed
+import android.os.Looper
+
 
 /**
  * @author dengxijian
  * @date 2019-08-11
  */
-class RecycleViewAdapter constructor(context : Context, startDragListener: OnStartDragListener, dataList : ArrayList<String>) : RecyclerView.Adapter<RecycleViewAdapter.MyViewHolder>()  {
+class RecycleViewAdapter constructor(
+    context: Context,
+    startDragListener: OnStartDragListener,
+    dataList: ArrayList<String>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    var mContext =  context
+    private var mContext = context
     private var mDataList = dataList
-    var mStartDragListener = startDragListener
+    private val normalType = 0     // 第一种ViewType，正常的item
+    private val footType = 1       // 第二种ViewType，底部的提示View
+    private var fadeTips = false  // 是否隐藏foot
+    private var hasMore = true   // 变量，是否有更多数据
+    private var mStartDragListener = startDragListener
+    private val mHandler = Handler(Looper.getMainLooper())
 
-    fun getDataList() : ArrayList<String> = mDataList
+    fun getDataList(): ArrayList<String> = mDataList
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):MyViewHolder{
-       val itemView = LayoutInflater.from(mContext).inflate(cn.dengxijian.demo.R.layout.item, parent,false)
-        return MyViewHolder(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == normalType) {
+            val itemView = LayoutInflater.from(mContext).inflate(cn.dengxijian.demo.R.layout.item, parent, false)
+            return MyItemViewHolder(itemView)
+        } else {
+            val itemView = LayoutInflater.from(mContext).inflate(cn.dengxijian.demo.R.layout.footview, parent, false)
+            return MyfootViewHolder(itemView)
+        }
     }
 
     override fun getItemCount(): Int = mDataList.size
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.name.setText(mDataList.get(position))
+    override fun onBindViewHolder(holderItem: RecyclerView.ViewHolder, position: Int) {
 
-        holder.name.setOnLongClickListener(View.OnLongClickListener { v ->
-            mStartDragListener.onStartDrag(holder)
-            true
-        })
+        if (holderItem is MyItemViewHolder) {
+            holderItem.name.setText(mDataList.get(position))
+
+            holderItem.name.setOnLongClickListener(View.OnLongClickListener { v ->
+                mStartDragListener.onStartDrag(holderItem)
+                true
+            })
+        } else {
+            if(!fadeTips) {
+                mHandler.post(Runnable {
+                    // 隐藏提示条
+                    (holderItem as MyfootViewHolder).name.setVisibility(View.VISIBLE)
+                })
+            }
+            if (hasMore) {
+                (holderItem as MyfootViewHolder).name.setText("正在加载更多...")
+            } else {
+                (holderItem as MyfootViewHolder).name.setText("没有更多数据了...")
+                //(holderItem as MyfootViewHolder).name.setVisibility(View.VISIBLE)
+                // 然后通过延时加载模拟网络请求的时间，在500ms后执行
+                mHandler.postDelayed(Runnable {
+                    // 隐藏提示条
+                    (holderItem as MyfootViewHolder).name.setVisibility(View.GONE)
+                    fadeTips = true
+                }, 500)
+
+            }
+        }
     }
 
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        var name : TextView = itemView.findViewById(cn.dengxijian.demo.R.id.textItem)
+    // 暴露接口，改变fadeTips的方法
+    fun isFadeTips(): Boolean {
+        return fadeTips
+    }
+
+    fun setFadeTips(fade: Boolean) {
+        fadeTips =fade
+    }
+    override fun getItemViewType(position: Int): Int {
+        if (position == mDataList.size - 1) {
+            return footType
+        } else {
+            return normalType
+        }
+    }
+
+    fun setHasMore(boolean: Boolean) {
+        hasMore = boolean
+    }
+    fun setInitData(list :ArrayList<String>){
+        mDataList = list
+    }
+
+
+
+    inner class MyItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var name: TextView = itemView.findViewById(cn.dengxijian.demo.R.id.textItem)
+    }
+
+    inner class MyfootViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var name: TextView = itemView.findViewById(cn.dengxijian.demo.R.id.tips)
     }
 }
